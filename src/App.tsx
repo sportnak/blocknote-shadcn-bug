9014466ba4d11251b342684ea8a074bfef829c93
sportnak <michael.amayakan@gmail.com>
2025-12-12 08:47:17 -0800

    unmount fail

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import { useState } from 'react';
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteSchema } from "@blocknote/core";
 // Or, you can use ariakit, shadcn, etc.
-import { BlockNoteView } from "@blocknote/shadcn";
+import { BlockNoteView } from "@blocknote/mantine";
 // Default styles for the mantine editor
 import "@blocknote/shadcn/style.css";
 // Include the included Inter font
 import "@blocknote/core/fonts/inter.css";
 import { createAlert } from "./Alert";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./components/ui/dropdown-menu";
 
 export default function App() {
+    const [showEditor, setShowEditor] = useState(true);
     // Create a new editor instance
     const schema = BlockNoteSchema.create().extend({
         blockSpecs: {
@@ -22,37 +23,14 @@ export default function App() {
         schema,
         initialContent: [
             {
-                type: "paragraph",
-                content: "Welcome to this demo!",
-            },
-            {
                 type: "alert",
                 content: "This is an example alert",
-            },
-            {
-                type: "paragraph",
-                content: "Click the '!' icon to change the alert type",
-            },
-            {
-                type: "paragraph",
             },
         ],
     });
 
     // Render the editor
     return <div>
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                Trigger
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-                align="start"
-            >
-                <DropdownMenuItem>
-
-                    Create new space
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-        <BlockNoteView editor={editor} /></div>;
+        <button onClick={() => setShowEditor(x => !x)}>click me</button>
+        {showEditor && <BlockNoteView editor={editor} />}</div>;
 }
